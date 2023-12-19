61d86f61df7ff9d33b37749360d1bd55cf88c49c
robertoxd3 <robertoaj0311@gmail.com>
2023-12-19 08:17:04 -0600

    enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
     apiUrl: 'http://192.168.19.37:4503/be/api/Ticket/',
     colaWebSocket: 'http://192.168.19.37:4503/be/Cola',
     colaWebSocket2: 'http://192.168.19.37:4503/be/Ticket',
+    colaWebSocket3: 'http://192.168.19.37:4503/be/Transferidos',
     colaUrl: 'http://192.168.19.37:4503/be/api/Cola/',
     loginUrl: 'http://192.168.19.37:4503/be/api/Authenticate',
     rutaVideos: 'http://192.168.30.21'
